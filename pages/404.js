df1814c8b6e67f3cf7b092bf995f58adbf87eda1
brunadalla <bruna.dall9741@gmail.com>
2023-04-18 13:31:46 -0300

    feat: base link component and 404 page

---
+++ pages/404.js
@@ -0,0 +1,10 @@
+import Link from "../src/components/Link"
+
+export default function Page404() {
+  return (
+    <div>
+      <h1>You got lost and ended here on page 404! </h1>
+      <Link href='/'> Go to Home page </Link>
+    </div>
+  )
+}
---
+++ pages/faq.js
@@ -0,0 +1,10 @@
+import Link from "../src/components/Link"
+
+export default function FAQPage() {
+  return (
+    <div>
+      <h1>Alura Cases - FAQ Page</h1>
+      <Link href='/'>Go to Home page</Link>
+    </div>
+  )
+}
