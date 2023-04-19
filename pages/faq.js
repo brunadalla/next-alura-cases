01729725d3b962cdf6ecb050333f8fbb82658d3d
brunadalla <bruna.dall9741@gmail.com>
2023-04-19 14:05:27 -0300

    getServerSideProps

--- pages/faq.js
+++ pages/faq.js
@@ -1,6 +1,6 @@
 import Link from "../src/components/Link"
 
-export async function getStaticProps() {
+export async function getServerSideProps() {
   const FAQ_API_URL =
     "https://gist.githubusercontent.com/omariosouto/0ceab54bdd8182cbd1a4549d32945c1a/raw/578ad1e8e5296fa048e3e7ff6b317f7497b31ad9/alura-cases-faq.json"
   const faq = await fetch(FAQ_API_URL)
