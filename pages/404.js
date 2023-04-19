17ede7b538825f14eaba16e4c1e6bcdbcf6ad9db
brunadalla <bruna.dall9741@gmail.com>
2023-04-19 14:55:22 -0300

    feat: faq screen

--- pages/404.js
+++ pages/404.js
@@ -5,8 +5,9 @@ export default function Page404() {
   return (
     <div>
       <PageTitle> 404 - Alura Cases </PageTitle>
-      <h1>You got lost and ended here on page 404! </h1>
-      <Link href='/'> Go to Home page </Link>
+      <h1>Você se perdeu e acabou caindo na página 404! </h1>
+      <Link href='/'> Voltar pra Home </Link>
     </div>
   )
 }
+
--- pages/faq.js
+++ pages/faq.js
@@ -1,34 +1,21 @@
-import Link from "../src/components/Link"
-import PageTitle from "../src/components/PageTitle"
+import FAQScreen from "../src/screens/FAQScreen"
 
-export async function getServerSideProps() {
+export default FAQScreen
+
+export async function getStaticProps() {
   const FAQ_API_URL =
     "https://gist.githubusercontent.com/omariosouto/0ceab54bdd8182cbd1a4549d32945c1a/raw/578ad1e8e5296fa048e3e7ff6b317f7497b31ad9/alura-cases-faq.json"
   const faq = await fetch(FAQ_API_URL)
-    .then((res) => res.json())
-    .then((res) => res)
+    .then((serverResponse) => {
+      return serverResponse.json()
+    })
+    .then((res) => {
+      return res
+    })
 
   return {
-    props: { faq },
+    props: {
+      faq,
+    },
   }
-}
-
-export default function FAQPage({ faq }) {
-  return (
-    <div>
-      <PageTitle> FAQ - Alura Cases </PageTitle>
-      <h1>Alura Cases - FAQ Page</h1>
-      <Link href='/'>Go to Home page</Link>
-      <ul>
-        {faq.map(({ answer, question }, index) => (
-          <li key={index}>
-            <article>
-              <h2>{question}</h2>
-              <p>{answer}</p>
-            </article>
-          </li>
-        ))}
-      </ul>
-    </div>
-  )
 }
